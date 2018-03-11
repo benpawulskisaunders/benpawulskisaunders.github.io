6a7e7d332a4ff359c322d6ea7f8abce872a76bac
benpawulskisaunders <benedictsaunders@btconnect.com>
2018-03-11 01:18:59 +0000

    Initial Commit
    We'll see what happens

---
+++ js/main.js
@@ -0,0 +1,65 @@
+﻿//Smooth scroll
+
+$(function () {
+
+    // Select all links with hashes
+    $('a[href*="#"]')
+        // Remove links that don't actually link to anything
+        .not('[href="#"]')
+        .not('[href="#0"]')
+        .click(function (event) {
+            // On-page links
+            if (
+                location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '')
+                &&
+                location.hostname == this.hostname
+            ) {
+                // Figure out element to scroll to
+                var target = $(this.hash);
+                target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+                // Does a scroll target exist?
+                if (target.length) {
+                    // Only prevent default if animation is actually gonna happen
+                    event.preventDefault();
+                    $('html, body').animate({
+                        scrollTop: target.offset().top - 113
+                    }, 1000, function () {
+                        // Callback after animation
+                        // Must change focus!
+                        var $target = $(target);
+                        $target.focus();
+                        if ($target.is(":focus")) { // Checking if the target was focused
+                            return false;
+                        } else {
+                            //$target.attr('tabindex', '-1'); // Adding tabindex for elements not focusable
+                            $target.focus(); // Set focus again
+                        };
+                    });
+                }
+            }
+        });
+
+    //Navbar appear
+$(document).ready(function () {
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 200) {
+            $('#top-menu').fadeIn(500);
+        } else {
+            $('#top-menu').fadeOut(500);
+        }
+    });
+    });
+
+$(document).ready(function () {
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 200) {
+            $('#to-top').fadeIn(500);
+        } else {
+            $('#to-top').fadeOut(500);
+        }
+    });
+});
+    })(jQuery);
+
+
+
